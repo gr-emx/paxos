8acaa70bcfbb4e2a829b30c6196e0788e2a9aa2c
Gaurav Raje <gaurav.raje07@gmail.com>
2019-02-03 18:42:58 -0500

    Initial commit

---
+++ src/main/java/com/gaurav/demo/PaxosApplication.java
@@ -0,0 +1,4 @@
+package com.gaurav.demo;
+
+public class PaxosApplication {
+}
---
+++ src/main/java/com/gaurav/demo/model/MessageRequest.java
@@ -0,0 +1,13 @@
+package com.gaurav.demo.model;
+
+public class MessageRequest {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
